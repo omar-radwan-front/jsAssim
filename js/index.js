8ae094acd1c974035a83b15fb2b9f2a11023f85c
omar-radwan-front <orayan491@gmail.com>
2025-05-30 21:00:00 +0300

    first commit

---
+++ js/index.js
@@ -0,0 +1,59 @@
+ 
+  var addQuotes =[`  “Be yourself; everyone else is already taken.”
+― Oscar Wilde ` ,
+ `“I'm selfish, impatient and a little insecure. I make mistakes, I am out of control and at times hard to handle. But if you can't handle me at my worst, then you sure as hell don't deserve me at my best.”
+― Marilyn Monroe`,
+`“So many books, so little time.”
+― Frank Zappa`,
+`“Two things are infinite: the universe and human stupidity; and I'm not sure about the universe.”
+― Albert Einstein`,
+`“A room without books is like a body without a soul.”
+― Marcus Tullius Cicero`,
+`“Be who you are and say what you feel, because those who mind don't matter, and those who matter don't mind.”
+― Bernard M. Baruch`
+]
+var lastIndex =-1;
+function displayQuote() {
+    var randomIndex =Math.floor(Math.random() * addQuotes.length);;
+  if (randomIndex === lastIndex) {
+    randomIndex = (randomIndex + 1) % addQuotes.length;
+  }
+
+  lastIndex = randomIndex;
+  var quote = addQuotes[randomIndex];
+  document.getElementById("demo").innerHTML = quote;
+}
+
+ 
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
